cdb5ebf066bbadb0a4c6c3a1284ef475aadf2873
LucianRain <aidanbacon@gmail.com>
2022-04-12 11:36:19 -0700

    test commit

--- src/main.js
+++ src/main.js
@@ -10,10 +10,3 @@ let borderPadding = borderUISize/3;
 let game = new Phaser.Game(config);
 
 let keyF, keyR, keyLEFT, keyRIGHT;
-
-/*
-    Display the time remaining (in seconds) on the screen (10)
-    Implement a simultaneous two-player mode (30)
-    Redesign the game's artwork, UI, and sound to change its theme/aesthetic (to something other than sci-fi) (60)
-
-*/
