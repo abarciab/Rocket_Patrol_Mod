3ecde2ec23eb34658b4203d51961d2491c791c6a
LucianRain <aidanbacon@gmail.com>
2022-04-14 16:16:53 -0700

    colmmit for kylan

--- src/main.js
+++ src/main.js
@@ -5,7 +5,7 @@ let config = {
     physics: {
         default: 'arcade',
         arcade: {
-            debug: true,
+            debug: false,
             gravity: {y:0}
         }
     },
@@ -19,21 +19,3 @@ let borderPadding = borderUISize/3;
 const game = new Phaser.Game(config);
 
 let keyF, keyR, keyLEFT, keyRIGHT, keySPACE, keySendWagonHigh, keySendWagonMid, keySendWagonLow, keyESC;
-
-/*
-    XXX Display the time remaining (in seconds) on the screen (10) XXX
-    Implement a simultaneous two-player mode (30)
-        p1 
-            fires arrows 
-            XX has a cooldown XX
-        p2
-            sends out wagons
-            
-            gets points for every wagon that gets to the other side
-    Redesign the game's artwork, UI, and sound to change its theme/aesthetic (to something other than sci-fi) (60)
-        starfield -> grassy meadow
-        ships -> horses and wagons
-        player -> archer
-        rockets -> arrows
-
-*/
